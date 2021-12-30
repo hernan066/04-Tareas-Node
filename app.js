b59ec6a0280e4f3006c951829801a905de5f2987
hernangit config --global user.name John <hernan0066@gmail.com>
2021-12-29 21:43:33 -0300

    4.46

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 require('colors');
+console.clear();
 
 const main = async() => {
     console.log('Hola mundo');
+
+    mostrarMenu();
+    pausa();
 }
 
 main();
